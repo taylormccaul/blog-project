48ed16e64c98fb21d2da9370ec0fd81501535c0c
Taylor <paigeemccaul@gmail.com>
2019-11-07 15:41:13 -0500

    Add delete and edit functions

--- blogger/app/controllers/articles_controller.rb
+++ blogger/app/controllers/articles_controller.rb
@@ -19,4 +19,22 @@ class ArticlesController < ApplicationController
 
     redirect_to article_path(@article)
   end
+
+  def edit
+    @article = Article.find(params[:id])
+  end
+
+  def destroy
+    @article = Article.find(params[:id])
+    @article.destroy
+
+    redirect_to article_path(@article)
+  end
+
+  def update
+    @article = Article.find(params[:id])
+    @article.update(article_params)
+
+    redirect_to article_path(@article)
+  end
 end
