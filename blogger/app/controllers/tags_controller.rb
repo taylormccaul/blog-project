e8f546b6163e74f4063347f735d3b4cc9bd8ab1f
Taylor <paigeemccaul@gmail.com>
2019-11-07 20:45:28 -0500

    Fixed tag bugs

--- blogger/app/controllers/tags_controller.rb
+++ blogger/app/controllers/tags_controller.rb
@@ -6,4 +6,13 @@ class TagsController < ApplicationController
   def show
     @tag = Tag.find(params[:id])
   end
+
+  def destroy
+    @tag = Tag.find(params[:id])
+    @tag.destroy
+
+    flash.notice = "Tag '#{@tag.name}' has been deleted."
+
+    redirect_to tags_path
+  end
 end
